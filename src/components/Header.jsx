d50db61afb07a6bb7b7fe3fd7aaf732368032abd
Justine Pain <painjustinepro@gmail.com>
2020-10-29 16:23:02 +0100

    Add state menu button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,78 +1,75 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Logo from '../img/logo-image.png';
 import './Header.css';
 
-/* const navButton = document.getElementById('menu-button');
-const navList = document.getElementById('nav-list');
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: props.active,
+    };
+  }
 
-function unrollMenu() {
-  navList.style.height = 'unset';
-  navList.style.height = `0`;
-}
-
-function rollMenu() {
-  navList.style.height = `0`;
-}
-
-let menuActive = false;
-
-navButton.addEventListener('click', () => {
-  if (menuActive) rollMenu();
-  else unrollMenu();
-
-  menuActive = !menuActive;
-});
-*/
-
-function Header() {
-  return (
-    <nav className="header">
-      <img className="logo" src={Logo} alt=" " />
-      <h1 className="header-title">Hearthstone Finder</h1>
-      <button
-        type="button"
-        className="menu-button"
-        aria-label="Open the navbar"
-      >
-        <span className="icon-menu" aria-hidden="true">
-          ☰
-        </span>
-      </button>
-      <ul className="nav-list">
-        <li className="nav-list-item">
-          <a className="nav-link" href="nav1.html">
-            Advanced search
-          </a>
-        </li>
-        <li className="nav-list-item">
-          <a className="nav-link" href="nav2.html">
-            Random card{' '}
-            <span role="img" aria-label="emoji">
-              🎲
-            </span>
-          </a>
-        </li>
-        <li className="nav-list-item">
-          <a className="nav-link" href="nav3.html">
-            Deck building
-          </a>
-        </li>
-        <li className="nav-list-item">
-          <a className="nav-link" href="nav4.html">
-            Tournaments
-          </a>
-        </li>
-      </ul>
-      <div className="search-bar">
-        <input type="search" id="search" />
-        <button type="button" id="search-button">
-          <span role="img" aria-label="emoji">
-            🔍
+  render() {
+    const { active } = this.state;
+    return (
+      <nav className="header">
+        <img className="logo" src={Logo} alt=" " />
+        <h1 className="header-title">Hearthstone Finder</h1>
+        <button
+          type="button"
+          className="Rollmenu"
+          aria-label="Open the navbar"
+          onClick={() => {
+            const newRollMenu = !active;
+            this.setState({ active: newRollMenu });
+          }}
+        >
+          <span className="icon-menu" aria-hidden="true">
+            ☰
           </span>
         </button>
-      </div>
-    </nav>
-  );
+        <ul className={active ? 'nav-list-active' : 'nav-list-inactive'}>
+          <li className="nav-list-item">
+            <a className="nav-link" href="nav1.html">
+              Advanced search
+            </a>
+          </li>
+          <li className="nav-list-item">
+            <a className="nav-link" href="nav2.html">
+              Random card{' '}
+              <span role="img" aria-label="emoji">
+                🎲
+              </span>
+            </a>
+          </li>
+          <li className="nav-list-item">
+            <a className="nav-link" href="nav3.html">
+              Deck building
+            </a>
+          </li>
+          <li className="nav-list-item">
+            <a className="nav-link" href="nav4.html">
+              Tournaments
+            </a>
+          </li>
+        </ul>
+        <div className="search-bar">
+          <input type="search" id="search" />
+          <button type="button" id="search-button">
+            <span role="img" aria-label="emoji">
+              🔍
+            </span>
+          </button>
+        </div>
+      </nav>
+    );
+  }
 }
+
+Header.propTypes = {
+  active: PropTypes.bool.isRequired,
+};
 
 export default Header;
