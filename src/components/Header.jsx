0d4e718eba57243e7c1a57d1ba985ecd57cbf25a
Justine Pain <painjustinepro@gmail.com>
2020-11-02 18:54:57 +0100

    Changed logo and down search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Logo from '../assets/logo-image.png';
+import Logo from '../assets/testlogo2.png';
 import './Header.css';
 
 class Header extends React.Component {
