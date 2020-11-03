e9530488dc6972a6e2e5946a086e46b8652b5d26
Vivien Michelesi <vivien.michelesi@hotmail.fr>
2020-11-03 17:24:26 +0100

    Premier rendu + App.test mute

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-<<<<<<< HEAD
 import PagePrinc from './components/PagePrinc';
-=======
-import Articles from './components/Articles';
 import Research from './components/Research';
->>>>>>> dev
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header active={false} />
-<<<<<<< HEAD
-      <PagePrinc />;
-=======
-      <Articles />
+      <PagePrinc />
       <Research />
->>>>>>> dev
       <Footer />
     </div>
   );
