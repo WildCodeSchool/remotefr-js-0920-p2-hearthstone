408311a4cc9c5a898561555571823ba3cd156928
Vivien Michelesi <vivien.michelesi@hotmail.fr>
2020-11-03 14:39:07 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header active={false} />
-      <Articles />;{/* <Switch>
-        <Route path="" />
-      </Switch> */}
+      <Articles />
       <Research />
       <Footer />
     </div>
