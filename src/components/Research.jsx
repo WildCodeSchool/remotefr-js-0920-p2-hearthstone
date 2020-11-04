b0164498f275c6b719afdc65c646acff9b493cf1
Justine Pain <painjustinepro@gmail.com>
2020-11-04 13:02:38 +0100

    Created Research content with filters and catalogue

--- src/components/Research.jsx
+++ src/components/Research.jsx
@@ -3,21 +3,26 @@ import './Research.css';
 
 function Research() {
   return (
-    <div className="Research">
-      <section className="filters">
-        <div className="class-card">
-          Demon hunter Druid Hunter Magus Paladin Priest Thief Shaman Warlock
-          Warrior Neutral
-        </div>
-        <div className="mana">1-10+</div>
-        <div className="attack">1-10+</div>
-        <div className="health">1-10+</div>
-        <div className="rareness">Commune Free Rare Epic Legendary</div>
-      </section>
-      <section className="cards-list">
-        <div>Catalogue</div>
-      </section>
-    </div>
+    <main className="research-container">
+      <div className="research-content">
+        <h1>Advanced Research</h1>
+
+        <section className="filters">
+          <div className="class-card">
+            Demon hunter Druid Hunter Magus Paladin Priest Thief Shaman Warlock
+            Warrior Neutral
+          </div>
+          <div className="mana">1-10+</div>
+          <div className="attack">1-10+</div>
+          <div className="health">1-10+</div>
+          <div className="rareness">Commune Free Rare Epic Legendary</div>
+        </section>
+
+        <section className="cards-list">
+          <div>Catalogue</div>
+        </section>
+      </div>
+    </main>
   );
 }
 
