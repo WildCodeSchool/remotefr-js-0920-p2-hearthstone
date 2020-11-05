aba9b5849e7a60191d90d9592cba3943e646e7f2
Justine Pain <painjustinepro@gmail.com>
2020-11-05 09:11:29 +0100

    Added mana cost button

--- src/components/Research.jsx
+++ src/components/Research.jsx
@@ -1,29 +1,79 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Research.css';
 
-function Research() {
-  return (
-    <main className="research-container">
-      <div className="research-content">
-        <h1>Advanced Research</h1>
+class Research extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: props.selected,
+    };
+  }
 
-        <section className="filters">
-          <div className="class-card">
-            Demon hunter Druid Hunter Magus Paladin Priest Thief Shaman Warlock
-            Warrior Neutral
-          </div>
-          <div className="mana">1-10+</div>
-          <div className="attack">1-10+</div>
-          <div className="health">1-10+</div>
-          <div className="rareness">Commune Free Rare Epic Legendary</div>
-        </section>
+  render() {
+    const { selected } = this.state;
+    return (
+      <main className="research-container">
+        <div className="research-content">
+          <h1>Advanced Research</h1>
 
-        <section className="cards-list">
-          <div>Catalogue</div>
-        </section>
-      </div>
-    </main>
-  );
+          <section className="filters">
+            <div className="class-card">
+              <form>
+                <select name="class" size="1">
+                  <option>Classes</option>
+                  <option>Demon hunter</option>
+                  <option>Druid</option>
+                  <option>Hunter</option>
+                  <option>Magus</option>
+                  <option>Paladin</option>
+                  <option>Priest</option>
+                  <option>Thief</option>
+                  <option>Shaman</option>
+                  <option>Warlock</option>
+                  <option>Warrior</option>
+                  <option>Neutral</option>
+                </select>
+              </form>
+            </div>
+
+            <div className="rarities">
+              <form>
+                <select name="rarities" size="1">
+                  <option>Rarity</option>
+                  <option>Commune</option>
+                  <option>Free</option>
+                  <option>Rare</option>
+                  <option>Epic</option>
+                  <option>Legendary</option>
+                </select>
+              </form>
+            </div>
+
+            <div className="mana-button">
+              <button
+                type="button"
+                onClick={() => {
+                  const manaSelected = selected;
+                  this.setState({ selected: manaSelected });
+                }}
+              >
+                <p className={selected ? 'mana-selected' : 'mana'}>Mana</p>
+              </button>
+            </div>
+          </section>
+
+          <section className="cards-list">
+            <div>Catalogue</div>
+          </section>
+        </div>
+      </main>
+    );
+  }
 }
+
+Research.propTypes = {
+  selected: PropTypes.bool.isRequired,
+};
 
 export default Research;
