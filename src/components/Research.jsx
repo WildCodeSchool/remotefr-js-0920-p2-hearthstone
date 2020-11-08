cca954f490b479c2efb9535381ab2a72943a53fc
vivien leroux <vivienmichelesi@gmail.com>
2020-11-08 17:16:36 +0100

    commit pour changer de branch

--- src/components/Research.jsx
+++ src/components/Research.jsx
@@ -3,66 +3,48 @@ import DisplayCard from './components/DisplayCard';
 import React from 'react';
 import axios from 'axios';
 
-const sampleCard = {
-  name: 'Blowgill Sniper',
-  playerClass: 'Neutral',
-  rarity: 'Common',
-  cost: 2,
-  attack: 2,
-  health: 1,
-  text: '<b>Battlecry:</b> Deal 1 damage.',
-  type: 'Minion',
-  race: 'Murloc',
-  cardSet: 'Mean Streets of Gadgetzan',
-  flavor:
-    'Imagine how much further his darts would go if he had lungs instead of gills!',
-  artist: 'Tyler West Studio',
-  img:
-    'http://media.services.zam.com/v1/media/byName/hs/cards/enus/CFM_647.png',
-};
-
 class Research extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      card: sampleCard,
-    };
-    this.getCard = this.getCard.bind(this);
+    this.state = {};
   }
 
-  getCard() {
-    const config = {
-      method: 'get',
-      url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards',
+  componentDidMount() {
+    this.getCards();
+  }
+
+  getCards = async () => {
+    const options = {
+      method: 'GET',
+      url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/DS1_175',
       headers: {
-        'x-rapidapi-key': 'ad80027f46msh8c1a1ed7780b8ccp130a6fjsnd21e03ec0703',
+        'x-rapidapi-key': '49a3b1f4e2mshf415725f12dbc29p1fcee0jsnd6406242952a',
         'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com',
       },
     };
 
-    axios(config)
-      .then((response) => response.data)
-      .then((data) => {
-        console.log('axios:', data);
-        this.setState({
-          card: data[0],
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
+    const myData = await axios.request(options).then(DisplayCard (response) {
+      // return (response.data[0]);
+      console.log(response.data[0]);
+    });
 
-  render() {
+    this.setState({ items: myData });
+  };
+
+  render = () => {
+    const { name, rarity, img } = this.state.items;
+
+    console.log(img);
     return (
-      <div className="Research">
-        <DisplayCard card={this.state.card} />
-        <button type="button" onClick={this.card}>
-          Get card
-        </button>
+      <div>
+        <h1>{name}</h1>
+        <img
+          src="https://media.services.zam.com/v1/media/byName/hs/cards/enus/DS1_175.png"
+          alt="coucou"
+        />
       </div>
     );
-  }
+  };
 }
 
-export default Research;
+export default SingleCard;
