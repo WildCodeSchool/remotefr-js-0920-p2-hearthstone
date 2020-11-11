81c8cd228bc4889caeb5ae38dfda0d44e1635217
Justine Pain <painjustinepro@gmail.com>
2020-11-12 00:01:57 +0100

    Added class and mana icons

--- src/components/FilterClass.jsx
+++ src/components/FilterClass.jsx
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
-import ClassDruidImage from '../assets/Filters-images/druid-unchecked.png';
+import ClassDemonHunterFilter from '../assets/Filters-images/demonhunter-unchecked.png';
+import ClassDruidFilter from '../assets/Filters-images/druid-unchecked.png';
+import ClassHunterFilter from '../assets/Filters-images/hunter-unchecked.png';
+import ClassMageFilter from '../assets/Filters-images/mage-unchecked.png';
+import ClassNeutralFilter from '../assets/Filters-images/neutral-unchecked.png';
+import ClassPaladinFilter from '../assets/Filters-images/paladin-unchecked.png';
+import ClassPriestFilter from '../assets/Filters-images/pretre-unchecked.png';
+import ClassRogueFilter from '../assets/Filters-images/rogue-unchecked.png';
+import ClassShamanFilter from '../assets/Filters-images/shaman-unchecked.png';
+import ClassWarlockFilter from '../assets/Filters-images/warlock-unchecked.png';
+import ClassWarriorFilter from '../assets/Filters-images/warrior-unchecked.png';
 
 class FilterClass extends Component {
   constructor(props) {
@@ -19,10 +29,20 @@ class FilterClass extends Component {
 
   render() {
     return (
-      <div className="filterClassContainer">
+      <div className="class-filter-container">
         <h2>Filter by class</h2>
         <div>
-          <img className="demon-hunter-filter" src={ClassDruidImage} alt=" " />
+          <img className="class-filter" src={ClassDruidFilter} alt=" " />
+          <img className="class-filter" src={ClassHunterFilter} alt=" " />
+          <img className="class-filter" src={ClassDemonHunterFilter} alt=" " />
+          <img className="class-filter" src={ClassMageFilter} alt=" " />
+          <img className="class-filter" src={ClassNeutralFilter} alt=" " />
+          <img className="class-filter" src={ClassPaladinFilter} alt=" " />
+          <img className="class-filter" src={ClassPriestFilter} alt=" " />
+          <img className="class-filter" src={ClassRogueFilter} alt=" " />
+          <img className="class-filter" src={ClassShamanFilter} alt=" " />
+          <img className="class-filter" src={ClassWarlockFilter} alt=" " />
+          <img className="class-filter" src={ClassWarriorFilter} alt=" " />
         </div>
       </div>
     );
--- src/components/FilterRarity.jsx
+++ src/components/FilterRarity.jsx
@@ -1,10 +1,32 @@
 import React, { PureComponent } from 'react';
+import Mana0Filter from '../assets/Filters-images/mana0-unchecked.png';
+import Mana1Filter from '../assets/Filters-images/mana1-unchecked.png';
+import Mana2Filter from '../assets/Filters-images/mana2-unchecked.png';
+import Mana3Filter from '../assets/Filters-images/mana3-unchecked.png';
+import Mana4Filter from '../assets/Filters-images/mana4-unchecked.png';
+import Mana5Filter from '../assets/Filters-images/mana5-unchecked.png';
+import Mana6Filter from '../assets/Filters-images/mana6-unchecked.png';
+import Mana7Filter from '../assets/Filters-images/mana7-unchecked.png';
+import Mana8Filter from '../assets/Filters-images/mana8-unchecked.png';
+import Mana9Filter from '../assets/Filters-images/mana9-unchecked.png';
 
 class FilterRarity extends PureComponent {
   render() {
     return (
       <div>
         <h2>Filter by rarity</h2>
+        <div>
+          <img className="mana-filter" src={Mana0Filter} alt=" " />
+          <img className="mana-filter" src={Mana1Filter} alt=" " />
+          <img className="mana-filter" src={Mana2Filter} alt=" " />
+          <img className="mana-filter" src={Mana3Filter} alt=" " />
+          <img className="mana-filter" src={Mana4Filter} alt=" " />
+          <img className="mana-filter" src={Mana5Filter} alt=" " />
+          <img className="mana-filter" src={Mana6Filter} alt=" " />
+          <img className="mana-filter" src={Mana7Filter} alt=" " />
+          <img className="mana-filter" src={Mana8Filter} alt=" " />
+          <img className="mana-filter" src={Mana9Filter} alt=" " />
+        </div>
       </div>
     );
   }
