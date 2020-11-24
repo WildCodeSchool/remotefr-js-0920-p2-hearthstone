c1125fef307421a12316965934f50725764c0b56
Justine Pain <painjustinepro@gmail.com>
2020-11-24 18:53:44 +0100

    Created tourmanents and deck buildings components

---
+++ src/components/DeckBuilding.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './DeckBuilding.css';
+
+const DeckBuilding = () => {
+  return (
+    <main className="background">
+      <div className="container">
+        <h1>Deck building</h1>
+        <p>Deck building will be available soon !</p>
+      </div>
+    </main>
+  );
+};
+
+export default DeckBuilding;
