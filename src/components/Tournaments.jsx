9fba859972912cf7088cd7ed1ec8b636236c2a2e
Justine Pain <painjustinepro@gmail.com>
2020-11-25 13:29:44 +0100

    Added map in component

--- src/components/Tournaments.jsx
+++ src/components/Tournaments.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
 import './Tournaments.css';
 
-const Tournaments = () => {
+function Map() {
   return (
-    <main className="background">
-      <div className="container">
-        <h1>Tournaments</h1>
-      </div>
-    </main>
+    <GoogleMap
+      defaultZoom={9}
+      defaultCenter={{ lat: 51.507351, lng: -0.127758 }}
+    />
   );
-};
+}
 
-export default Tournaments;
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <WrappedMap
+        googleMapURL="https://maps.googleapis.com/maps/api/js?=3.exp&libraries=geometry,drawing,places&key=AIzaSyCZOhbYBKBS6Gw-G_xebAbRMMmukKzGYWg"
+        loadingElement={<div style={{ height: '100%' }} />}
+        containerElement={<div style={{ height: '100%' }} />}
+        mapElement={<div style={{ height: '100%' }} />}
+      />
+    </div>
+  );
+}
+
+// AIzaSyC - epvj9mmg0_iIrGm0tzIdZSADCoLlPd4;
+// AIzaSyCZOhbYBKBS6Gw - G_xebAbRMMmukKzGYWg;
